02fba1bd419c4c82ce84f734c659937c07b25238
Ramzi Abdoch <ramzi.abdoch@gmail.com>
2014-10-03 04:25:01 -0400

    timeline

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-function explosions(){
-	window.alert("swag hard, young padawan")
+function onLoad(){
+
 }
+
+var Engine = require('famous/core/Engine');
+var Surface = require('famous/core/Surface');
+
+var mainContext = Engine.createContext();
+
+var firstSurface = new Surface({
+  content: 'hello world'
+});
+
+mainContext.add(firstSurface);
